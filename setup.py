1650990e075e60f9ecdeec1160f4265ff59c9b45
John MacKenzie <john@nineteeneightd.com>
2012-03-22 17:49:18 -0500

    setup.py
    Add setuptools setup file for building and installing the sclara
    package. Update gitignore to get rid of setuptools artifacts.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sclara',
+      version='0.0.1',
+      description='Sclara is a Python testing DSL.',
+      author='John MacKenzie',
+      author_email='john@nineteeneightd.com',
+      url='http://github.com/198d/sclara',
+      install_requires=['greenlet >=0.3.4,<0.4.0'],
+      packages=['sclara'])
